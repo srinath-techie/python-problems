357d1f5778b3a393c0a9a405c8999ee827b95fc8
srinath-techie <srinath2020m@gmail.com>
2024-08-04 17:33:05 +0530

    Add files via upload

---
+++ shopping.py
@@ -0,0 +1,19 @@
+print("Welocmes you")
+print(" @@@ Here is your Menu @@@")
+print (" Pen - 5rs\n Pencil - 3rs\n Sharpener - 2rs\n Eraser - 1re")
+print("pls select what you want")
+Thing_1 = int(input("Pen"))
+num = Thing_1
+Thing_2 = int(input("Pencil"))
+num = Thing_2
+Thing_3 = int(input("Sharpener"))
+num = Thing_3
+Thing_4 = int(input("Eraser"))
+num = Thing_4
+print("total",Thing_1+Thing_2+Thing_3+Thing_4)
+'''Thing_1=5*num
+Thing_2=3*num
+Thing_3=2*num
+Thing_4=1*num
+amount=Thing_1+Thing_2+Thing_3+Thing_4
+print("total amount=",amount)'''
